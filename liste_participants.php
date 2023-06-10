54d4f83d1a973830701d5dfdddf9aae8ebdbd533
Salissou01 <99973298+Salissou01@users.noreply.github.com>
2023-06-10 17:04:05 +0100

    Add files via upload

--- liste_participants.php
+++ liste_participants.php
@@ -1,6 +1,11 @@
 <?php
+session_start();
+if(!isset($_SESSION['login'])){//Si la variable session n'a pas été créee
+    header("location:index.php");
+    exit();
+}
 //Appel du fichier de connexion à la bd
-require_once('connexion_db/conn_db.php');
+require_once('../connexion_db/conn_db.php');
 //Définition de la requête de sélection
 $sql_part="select * from participant natural join societe";
 //Exécution
@@ -11,16 +16,16 @@ $query_part=$pdo->query($sql_part);
 <head>
     <meta charset="utf-8">
     <title>Liste des participants</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="styletable.css">
-    
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../styletable.css">
 </head>
 <body>
-    <?php include "menu.php"; ?>
+    <?php include"menu.php" ?>
 <table>
     <caption>Liste des participants</caption>
     <tr>
-        <th>ID</th>
+        <th>Modification</th>
+        <th>Suppression</th>
         <th>NOM</th>
         <th>Prénoms</th>
         <th>E-mail</th>
@@ -31,7 +36,9 @@ $query_part=$pdo->query($sql_part);
     while($part=$query_part->fetch(PDO::FETCH_ASSOC)){//Tant qu'on extrait des lignes sous forme de tableau associatif
         extract($part);
         echo"<tr>
-                <td>$id</td>
+                <td><a href='fiche_participant.php?id=$id'>Editer</a></td>
+                <td><a href='supprim_participant.php?id=$id'
+                    onclick=\"return confirm('Voulez vous supprimer $nom ? Oui ou Non?');\">Supprimer</a></td>
                 <td>$nom</td>
                 <td>$prenom</td>
                 <td>$email</td>
--- menu.php
+++ menu.php
@@ -1,8 +1,7 @@
 <div id="menu">
 <ul>
-    <li><a href="index.php">Accueil</a></li>
-    <li><a href="form_inscription.php">Inscription</a></li>
+    <li><a href="../index.php">Accueil</a></li>
     <li><a href="liste_participants.php">liste des participants</a></li>
-    <li><a href="admin/index.php">Administration</a></li>
+    <li><a href="sortie.php">Déconnexion</a></li>
 </ul>
 </div>
